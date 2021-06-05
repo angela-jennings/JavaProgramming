4ce4c669aaa9d55d082a1295c41fbeac1d3970ae
angela-jennings <amjennings118@gmail.com>
2021-06-05 09:05:20 -0600

    created 2 new classes - starting topic of static block

---
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -0,0 +1,7 @@
+package day50_inheritance;
+
+public class StaticBlockDemo {
+    static{
+        System.out.println("static initializer block");
+    }
+}
---
+++ src/day50_inheritance/StaticBlockTest.java
@@ -0,0 +1,7 @@
+package day50_inheritance;
+
+public class StaticBlockTest {
+    public static void main(String[] args) {
+        StaticBlockDemo st1 = new StaticBlockDemo();
+    }
+}
