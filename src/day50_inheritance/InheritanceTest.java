06d0072e7d2b5ccde7311a57d1e9755f865d85ee
angela-jennings <amjennings118@gmail.com>
2021-06-05 19:21:06 -0600

    created 3 classes - talking about inheritance

---
+++ src/day50_inheritance/InheritanceTest.java
@@ -0,0 +1,22 @@
+package day50_inheritance;
+
+public class InheritanceTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Angela";
+        p1.age = 31;
+
+        p1.talk();
+        p1.walk();
+        p1.work("SDET");
+
+        Teacher t1 = new Teacher();
+        t1.teacherID = 123456;
+        t1.teach("Coding Java");
+        t1.name = "Koga";
+        t1.age = 10;
+        t1.talk();
+        t1.walk();
+        t1.work("Cuddle Buddy");
+    }
+}
