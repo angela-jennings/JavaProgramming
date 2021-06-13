907601ff8815df57929d87ac782e512f6e97166e
angela-jennings <amjennings118@gmail.com>
2021-06-12 19:53:39 -0600

    changes variables to private to show how encapsulated data can be called through inheritance

--- src/day52_inheritance/AppObjects.java
+++ src/day52_inheritance/AppObjects.java
@@ -3,18 +3,20 @@ package day52_inheritance;
 public class AppObjects {
     public static void main(String[] args) {
         MobileApp mobileApp = new MobileApp();
-        mobileApp.name = "Youtube";
+        mobileApp.setName("Youtube");
         mobileApp.useTheApp(10);
         System.out.println();
 
         Instagram instagram = new Instagram();
-        instagram.name = "Instagram";
+        instagram.setName("Instagram");
         instagram.useTheApp(20);
         System.out.println();
 
         Discord discord = new Discord();
-        discord.name = "Discord";
+        discord.setName("Discord");
         discord.chat("Java");
         discord.useTheApp(15);
+        discord.setVersion(1.25);
+        discord.download();
     }
 }
