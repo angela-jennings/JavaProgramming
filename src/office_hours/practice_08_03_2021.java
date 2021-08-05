99ba2a80e7db6a93ae0ffda8b458cff1ee3af0f6
angela-jennings <amjennings118@gmail.com>
2021-08-05 10:32:45 -0600

    office hours

---
+++ src/office_hours/practice_08_03_2021.java
@@ -0,0 +1,27 @@
+package office_hours;
+
+import java.util.*;
+
+public class practice_08_03_2021 {
+    public static void main(String[] args) {
+        Collection<String> list1 = new ArrayList<>(); //polymorphic
+        List<String> list2 = new ArrayList<>(); //polymorphic
+        ArrayList <String> list3 = new ArrayList<>(); //regular way
+
+        list2.add("Koga");
+        list2.add("Athena");
+        list2.add("Hubby");
+
+        List<String> list4 = new Stack<>();
+
+        list4.add("Koga");
+        list4.add("Athena");
+        list4.add("Hubby");
+
+
+
+
+
+
+    }
+}
